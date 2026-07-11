42db27ee4dc4cffcf59ec1de2ca97e9b8e25a996
samirjungthapa <samirjungthapa@users.noreply.github.com>
2026-07-11 14:50:55 +0545

    chore: Make phone field optional in User model

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  phone: { type: String, required: true },
+  phone: { type: String },
   isVerified: { type: Boolean, default: false },
   otp: { type: String },
   otpExpires: { type: Date },
